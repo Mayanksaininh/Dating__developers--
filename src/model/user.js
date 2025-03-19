09f4127c2f856b2ed873676c05158ecc61b7fe85
Mayanksaininh <mayanksaininh@gmail.com>
2025-03-19 15:14:14 +0530

    Define Schema

--- src/model/user.js
+++ src/model/user.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    
+    firstName : {
+        type : String
+    },
+    lastName : {
+        type : String
+    },
+    emialID : {
+        type : String
+    },
+    password : {
+        type : String
+    },
+    age : {
+        type : Number
+    },
+    gender : {
+        type : String
+    }
 })
