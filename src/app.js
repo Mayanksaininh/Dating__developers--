a913c3e536ebf2334a6a13e970c094f51fa7ada0
Mayanksaininh <mayanksaininh@gmail.com>
2025-03-23 18:38:53 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -2,12 +2,36 @@ const express = require('express');
 
 const app = express();
 const connectdb = require("./database")
+const user = require("./model/user")
+// for add user into the database
 
+app.use(express.json())
+
+app.post("/signup" , async(req, res) =>{ 
+
+    const user = new user (req.body);
+
+// creating a new instance of the user model
+//     const user = new user( {
+//         firstName : 'Mayank',
+//         lastName : 'saini',
+//         email : 'mayank@saini.com',
+//         password : 'mayank@123',
+//     })
+
+   try {await user.save() 
+   res.send("User added successfully")
+   }
+   catch{
+    res.status(404).send("user is not added");
+   }
+})
 // app.use('/', (req,res) => {
 //     res.send("server is UP and running");
 // })
 
-connectdb.then(()=>{
+connectdb()
+.then(()=>{
     console.log("Database connection established...")
     app.listen(3000, ()=>{
         console.log("Server is successfully listening on port 3000")
@@ -17,58 +41,57 @@ connectdb.then(()=>{
    console.error("Database can't be connected")
  })
 
+// app.use("/hello", ( req, res )=>{
+//     res.send("hello server");
+// })
 
-app.use("/hello", ( req, res )=>{
-    res.send("hello server");
-})
+// app.use("/ab?c", ( req, res )=>{
+//     res.send("hello form server")
+// })
 
-app.use("/ab?c", ( req, res )=>{
-    res.send("hello form server")
-})
+// app.use("/ab+c", ( req, res )=>{
+//     res.send("hello server")
+// })
 
-app.use("/ab+c", ( req, res )=>{
-    res.send("hello server")
-})
-
-app.use("/ab*c", ( req, res )=>{
-    res.send("hello form server")
-})
+// app.use("/ab*c", ( req, res )=>{
+//     res.send("hello form server")
+// })
 
 
-app.get("/user" , (req,res,next) =>{
-    // res.send({firstname : "mayank" , lastname : "saini"})
-    next();
-},
-(req,res)=>{
-    res.send("ush");
-    // res.send("welding");
-}
-)
+// app.get("/user" , (req,res,next) =>{
+//     // res.send({firstname : "mayank" , lastname : "saini"})
+//     next();
+// },
+// (req,res)=>{
+//     res.send("ush");
+//     // res.send("welding");
+// }
+// )
 
-app.post("/user" , (req,res) =>{
-    res.send("saved in DB succesfully")
-})
+// app.post("/user" , (req,res) =>{
+//     res.send("saved in DB succesfully")
+// })
 
-app.delete("/user", (req,res) =>{
-    res.send("user is deleted ")
-})
-const {adminauth}  = require("./middleware/auth")
+// app.delete("/user", (req,res) =>{
+//     res.send("user is deleted ")
+// })
+// const {adminauth}  = require("./middleware/auth")
 
-app.use("/use" , adminauth, (req,res) => {
-    res.send("df");
-})
+// app.use("/use" , adminauth, (req,res) => {
+//     res.send("df");
+// })
 
-app.get("/admin/data", (req,res) => {
-    res.send("Get all data")
-})
+// app.get("/admin/data", (req,res) => {
+//     res.send("Get all data")
+// })
 
-app.delete("/admin/deleteuser", (req,res) => {
-     // check the request is authenticated, if it is actually from the admin side
-     res.send("detele user")
-})
+// app.delete("/admin/deleteuser", (req,res) => {
+//      // check the request is authenticated, if it is actually from the admin side
+//      res.send("detele user")
+// })
 
-app.get("/" , (err,req,res,next) =>{
-    if(err){
-        res.status(500).send("Something went worng")
-    }
-})
+// app.get("/" , (err,req,res,next) =>{
+//     if(err){
+//         res.status(500).send("Something went worng")
+//     }
+// })
